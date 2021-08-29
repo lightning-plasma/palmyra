e4dd1e3a3294b46c7a9f2424dc0bf1b3f6cae2c4
iiohioryuki <hiroyuki.iio@askul.com>
2021-08-29 19:13:36 +0900

    main

--- src/main/kotlin/com/architype/palmyra/runner/Main.kt
+++ src/main/kotlin/com/architype/palmyra/runner/Main.kt
@@ -1,13 +1,19 @@
 package com.architype.palmyra.runner
 
 import com.architype.palmyra.converter.MyConverter
+import java.io.BufferedWriter
+import java.nio.charset.Charset
+import java.nio.file.Files
+import java.nio.file.Path
 
 fun main() {
     // U+FF5E などが含まれる文字列
     val source = "スチー～ル製￡軽中∥量～中量－棚（～部品・オ￠プ￠ション~）スチー～ル製￡軽中∥量～中量－棚（～部品・オ￠プ￠ション~）スチー～ル製￡軽中∥量～中量－棚（～部品・オ￠プ￠ション~）"
 
-    val convert1 = MyConverter.convert(source)
-    val convert2 = MyConverter.convertEach(source)
+    val writer: BufferedWriter =
+        Files.newBufferedWriter(Path.of("/tmp/foo.txt"), Charset.forName("Shift_JIS"))
 
-    println(convert1 == convert2)
+    val converted = MyConverter.convertEach(source)
+    writer.write(converted)
+    writer.close()
 }
